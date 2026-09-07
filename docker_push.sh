009d26a7de3481d332a6c4bef55ff01c14a12a90
Shriorange <shri80@gmail.com>
2026-09-07 19:46:37 -0400

    update 10

--- docker_push.sh
+++ docker_push.sh
@@ -2,8 +2,8 @@
 set -e
 
 ./mvnw clean install -DskipTests=true
-docker build -t simplybyte/simplybyte-backend:1.0 .
+docker build -t srorange123/springboot-backend:1.0 .
 
 echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USER" --password-stdin
 
-docker push simplybyte/simplybyte-backend:1.0
+docker push srorange123/springboot-backend:1.0
